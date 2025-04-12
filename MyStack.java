34c0d7254de351c1a7d82a35e748a371a0a42750
sym06m <symbat.otegen06@gmail.com>
2025-04-12 08:33:29 +0500

    Create MyStack.java

---
+++ MyStack.java
@@ -0,0 +1,29 @@
+public class MyStack<T> {
+    private MyArrayList<T> list;
+
+    public MyStack() {
+        this.list = new MyArrayList<>();
+    }
+
+    public void push(T item) {
+        list.addLast(item);
+    }
+
+    public T pop() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        return list.removeLast();
+    }
+
+    public T peek() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
